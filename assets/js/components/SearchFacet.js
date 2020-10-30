16e1ea0d5f01626de9949593a26333a9c11423ed
Derek Enos <derek.enos@gmail.com>
2020-10-30 10:19:57 -0400

    Added SearchFacetValue click handler

--- assets/js/components/SearchFacet.js
+++ assets/js/components/SearchFacet.js
@@ -77,10 +77,10 @@ class SearchFacetValues extends HTMLElement {
   }
 
   connectedCallback () {
-    // Read any specified default-num-visible attribute.
-    if (this.hasAttribute("default-num-visible")) {
+    // Read any specified initial-num-visible attribute.
+    if (this.hasAttribute("initial-num-visible")) {
       this.defaultNumVisible = parseInt(
-        this.getAttribute("default-num-visible")
+        this.getAttribute("initial-num-visible")
       )
     }
 
@@ -140,6 +140,13 @@ export class SearchFacet extends HTMLElement {
     // Define a flag to indicate whether the facet values are collapsed.
     this.collapsed = false
 
+    // Define an array for value toggle listener functions.
+    this.valueClickListeners = []
+
+    // Define properties that will be populated within connectedCallback.
+    this.name = undefined
+    this.displayName = undefined
+
     // Grab the <search-facet-values> element so that we can later manually
     // place it into its <slot>. Note that this would happen automatically if
     // we were using a shadow DOM.
@@ -148,6 +155,7 @@ export class SearchFacet extends HTMLElement {
     // Define the component's inner structure.
     this.innerHTML =
       `<h1 class="name">
+         <span class="name"></span>
          <span class="collapsed-icon">-</span>
        </h1>
        <!-- Define a slot for <search-facet-values> element -->
@@ -159,17 +167,19 @@ export class SearchFacet extends HTMLElement {
 
   connectedCallback () {
     // Read the custom element attributes.
-    const name = this.getAttribute("name")
+    this.name = this.getAttribute("name")
+    this.displayName = this.getAttribute("display-name")
 
     // Update the component with the attribute values.
-    this.querySelector('h1.name').insertBefore(
-      document.createTextNode(name),
-      this.querySelector('h1 > span.collapsed-icon')
-    )
+    this.querySelector("h1 > span.name").textContent = this.displayName
 
     // Register the facet header collapse click handler.
     this.querySelector("h1")
-        .addEventListener("click", this.toggleCollapsed.bind(this))
+      .addEventListener("click", this.toggleCollapsed.bind(this))
+
+    // Register the value click handler.
+    this.querySelector("search-facet-values")
+      .addEventListener("click", this.valueClickHandler.bind(this))
   }
 
   toggleCollapsed () {
@@ -185,6 +195,34 @@ export class SearchFacet extends HTMLElement {
     // display value of "block".
     this.querySelector("search-facet-values").style.display =
       this.collapsed ? "none" : "block"
+  }
+
+  addValueClickListener (fn) {
+    /* Add a function to the value click listeners array.
+     */
+    this.valueClickListeners.push(fn)
+  }
+
+  removeValueClickListener (fn) {
+    /* Remove a function from the value click listeners array.
+     */
+    for (const i in this.valueClickListeners) {
+      if (this.valueClickListeners[i] === fn) {
+        // Remove the function from the listeners array and return.
+        this.valueClickListeners.splice(i, 1)
+        return
+      }
+    }
+  }
+
+  valueClickHandler (e) {
+    /* Invoke all registered value click listeners.
+     */
+    let target = e.target.closest("search-facet-value")
+    const value = target.getAttribute("value")
+    for (let fn of this.valueClickListeners) {
+      fn(this.name, value)
+    }
   }
 }
 
