b19868cf521e5ad9320e8ee5c2afcf43a654a373
Derek Enos <derek.enos@gmail.com>
2020-12-02 10:05:19 -0500

    Added rollup-plugin-copy to copy in dev assets on build. Added @web/dev-server to package.json to make 'npm run start:build' work

--- _apps/search/rollup.config.js
+++ _apps/search/rollup.config.js
@@ -1,6 +1,6 @@
 import merge from 'deepmerge';
-// use createSpaConfig for bundling a Single Page App
 import { createSpaConfig } from '@open-wc/building-rollup';
+import copy from 'rollup-plugin-copy';
 
 // use createBasicConfig to do regular JS to JS bundling
 // import { createBasicConfig } from '@open-wc/building-rollup';
@@ -31,4 +31,12 @@ export default merge(baseConfig, {
   // alternatively, you can use your JS as entrypoint for rollup and
   // optionally set a HTML template manually
   // input: './app.js',
+
+  // Copied from: https://open-wc.org/docs/building/rollup/#copying-assets
+  plugins: [
+    copy({
+      targets: [{ src: 'assets/**/*', dest: 'dist/assets' }],
+      flatten: false,
+    })
+  ],
 });
